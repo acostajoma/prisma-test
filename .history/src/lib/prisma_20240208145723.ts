58e75b13a684c8a0487d3688751b78bbeaeb5113
Martin <acostajoma9@gmail..com>
2024-02-08 15:01:05 -0600

    Fixing issue with cloudflare

---
+++ .history/src/lib/prisma_20240208145723.ts
@@ -0,0 +1,7 @@
+// src/lib/prisma.ts
+
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const prisma = new PrismaClient().$extends(withAccelerate())
+export default prisma
